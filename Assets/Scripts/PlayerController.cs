d778605622c61d1d4c0e0937f13a29785f36cb03
FatimaMahfoodh <139788668+FatimaMahfoodh@users.noreply.github.com>
2024-04-01 17:31:30 +0300

    Added basic movement functions

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,20 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public InputManager InputManager;
+
+    private InputAction move;
+    private InputAction jump;
+    private InputAction switchRobot;
+    
+    private Rigidbody rb;
+
+     Vector2 moveDirection = Vector2.zero;
+    [SerializeField] float moveSpeed;
+
+    private void Awake()
     {
-        
+        InputManager = new InputManager();
+    }
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        move = InputManager.Player.Move;
+        move.Enable();
+
+        jump = InputManager.Player.Jump;
+        jump.Enable();
+
+        switchRobot = InputManager.Player.SwitchRobot;
+        switchRobot.Enable();
+    }
+
+    private void OnDisable()
+    {
+        move.Disable();
+        jump.Disable();
+        switchRobot.Disable();
+    }
+    private void Update()
+    {
+        moveDirection = move.ReadValue<Vector2>();
+
+    }
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector3(moveDirection.x * moveSpeed, 0, moveDirection.y * moveSpeed);
     }
 }
