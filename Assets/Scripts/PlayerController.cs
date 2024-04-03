b782d3bb7f7c61fee970c4e9a5a6d266233aa45e
FatimaMahfoodh <139788668+FatimaMahfoodh@users.noreply.github.com>
2024-04-03 13:26:14 +0300

    fixed player movement issue related to camera rotation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -17,8 +17,9 @@ public class PlayerController : MonoBehaviour
     Vector2 moveDirection = Vector2.zero;
     [SerializeField] float moveSpeed;
 
-    public Boolean isActive;
+   
 
+    [SerializeField] Camera camera;
     private void Awake()
     {
         InputManager = new InputManager();
@@ -52,27 +53,35 @@ public class PlayerController : MonoBehaviour
         InputManager.Player.Jump.performed -= OnJumpPerformed;
         InputManager.Player.Jump.canceled -= OnJumpCancelled;
     }
-    private void Update()
+    private void Update()   
     {
 
         
     }
     private void FixedUpdate()
     {
-        // Ensure that the moveDirection is normalized so diagonal movement isn't faster
-        Vector3 movement = new Vector3(moveDirection.x, 0.0f, moveDirection.y).normalized;
-
-        // Apply movement to the Rigidbody
-        rb.velocity = movement * moveSpeed;
+        Vector3 targetVector = new Vector3(moveDirection.x, 0.0f, moveDirection.y);
+        MovePlayer(targetVector);
     }
 
     private void OnMovePerformed(InputAction.CallbackContext context)
     {
         moveDirection = context.ReadValue<Vector2>();
+        
     }
     private void OnMoveCancelled(InputAction.CallbackContext context)
     {
         moveDirection = Vector2.zero;
+    }
+    void MovePlayer(Vector3 targetVector)
+    {
+        //Player Movement Speed
+        var speed = moveSpeed * Time.deltaTime;
+
+        targetVector = Quaternion.Euler(0, camera.gameObject.transform.eulerAngles.y, 0) * targetVector;
+
+        var targetPosition = transform.position + targetVector * speed;
+        transform.position = targetPosition;
     }
     void OnJumpPerformed(InputAction.CallbackContext context)
     {
