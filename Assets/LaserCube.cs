dd7311b8089b1cc46d589c7605095366ea2ad46c
Hasan <202101277@student.polytechnic.bh>
2024-05-02 14:06:51 +0300

    Refactored laser code Fixed laser cube lasers Fixed linerenderer when raycast detects nothing

--- Assets/LaserCube.cs
+++ Assets/LaserCube.cs
@@ -10,38 +10,77 @@ public class LaserCube : MonoBehaviour
     [SerializeField] 
     private Material blueMaterial;
 
+    public bool isBlue = false;
+
     private LineRenderer lineRenderer;
 
     [SerializeField]
     private Transform startPoint;
-    
+    private Laser laserScript;
+
     // Start is called before the first frame update
     void Start()
     {
         lineRenderer = GetComponent<LineRenderer>();
-        lineRenderer.SetPosition(0, startPoint.localPosition);
+        lineRenderer.SetPosition(0, startPoint.position);
         //lineRenderer.useWorldSpace = true;
         lineRenderer.startWidth = 0.4059853f;
         lineRenderer.endWidth = 0.4059853f;
         lineRenderer.enabled = false;
+
+
+        laserScript = GetComponent<Laser>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (laserScript.enabled)
+        {
+            lineRenderer.SetPosition(0,startPoint.position);
+            laserScript.direction = transform.forward;
+        }
     }
 
     public void RedirectLaser(Component sender, object data, int t)
     {
         Debug.Log("Redirect Laser called!");
-        ChangeInnerSphereColor(sender);
+        
         if (!lineRenderer.enabled)
         {
             lineRenderer.enabled = true;
+            laserScript.enabled = true;
+            laserScript.isBlue = isBlue;
+            laserScript.direction = transform.forward;
+            Debug.Log("Direction: "+laserScript.direction);
+            Debug.Log("Rotation: "+transform.forward);
         }
+        ChangeInnerSphereColor(sender);
 
-        //Debug.DrawLine(startPoint.position, new Vector3(0f, 0f, 15f), Color.red);
+        //Debug.Log("This is the starting point: " + startPoint.position);
+        //Debug.Log("This is the ending point: " + lineRenderer.GetPosition(1));
+        //Debug.DrawLine(startPoint.localPosition, lineRenderer.GetPosition(1), Color.blue);
+        //Debug.Log("This is the direction: " + (lineRenderer.GetPosition(1) - startPoint.localPosition));
+
+        //RaycastHit hit;
+        //if (Physics.Linecast(startPoint.position, lineRenderer.GetPosition(1), out hit))
+        //{
+
+        //    if (hit.collider)
+        //    {
+        //        Debug.Log("I have hitted something: " + hit.collider.tag);
+        //        lineRenderer.SetPosition(1, hit.point);
+        //    }
+        //}
+    }
+    public void StopLaser(Component sender, object data, int t)
+    {
+        Debug.Log("Stop Laser called!");
+        if (lineRenderer.enabled)
+        {
+            lineRenderer.enabled = false;
+            laserScript.enabled = false;
+        }
     }
 
     private void ChangeInnerSphereColor(Component sender)
@@ -51,10 +90,12 @@ public class LaserCube : MonoBehaviour
         {
             GameObject.Find("InnerSphere").gameObject.GetComponent<MeshRenderer>().material = blueMaterial;
             lineRenderer.material = blueMaterial;
+            isBlue = true;
         } else
         {
             GameObject.Find("InnerSphere").gameObject.GetComponent<MeshRenderer>().material = redMaterial;
             lineRenderer.material = redMaterial;
+            isBlue = false;
         }
         
     }
---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,127 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+
+    private LineRenderer lineRenderer;
+    public Transform startPoint;
+    public Vector3 direction;
+    public bool isBlue;
+    private int laserDistance = 1000;
+
+    [Header("Events")]
+    public GameEvent onLaserCollided;
+    public GameEvent onLaserBlocked;
+    public GameEvent onLaserCollidedWithLaserCube;
+
+    private GameObject[] laserReceivers;
+    private GameObject lastHittedRecevier;
+    private bool hasLaserBlockedBefore = false;
+
+    void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.SetPosition(0, startPoint.position);
+        GetSimilarReceviers();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //Debug.Log("This is the starting point: " + startPoint.position);
+        //Debug.Log("This is the ending point: " + lineRenderer.GetPosition(1));
+        //Debug.Log("This is the direction: " + (lineRenderer.GetPosition(1) - startPoint.localPosition));
+        //Debug.DrawRay(startPoint.position,direction*10, Color.blue, 3);
+        //Debug.Log(startPoint.position);
+        //lineRenderer.SetPosition(0, new Vector3(0, 0, 0));
+        RaycastHit hit;
+
+        if (Physics.Raycast(startPoint.position, direction, out hit))
+        {
+
+            if (hit.collider)
+            {
+                Debug.Log("I have hit by: " +  hit.collider);
+                lineRenderer.SetPosition(1, hit.point);
+            }
+            if (hit.transform.tag.EndsWith("LaserReceiver"))
+            {
+
+                if (laserReceivers.Contains(hit.transform.gameObject))
+                {
+                    lastHittedRecevier = hit.transform.gameObject;
+                    Debug.Log("Open Gate Logic!");
+                    onLaserCollided.Raise(this, null, lastHittedRecevier.gameObject.GetComponent<LaserReceiver>().GateNumber);
+                    hasLaserBlockedBefore = false;
+                    //Debug.Log("Event raiser with this color: " + hit.transform.gameObject.tag);
+
+                }
+                else
+                {
+                    lastHittedRecevier = null;
+                    Debug.Log("Wrong receiver color" + lastHittedRecevier);
+                }
+
+            }
+            else
+            {
+                //Debug.Log("Close Gate Logic!");
+                //Debug.Log("laser emitter gate number attached: " + laserReceivers[0].gameObject.GetComponent<LaserReceiver>().GateNumber);
+
+                if (!hasLaserBlockedBefore && lastHittedRecevier != null)
+                {
+
+                    onLaserBlocked.Raise(this, null, 1);
+                    lastHittedRecevier = null;
+                    hasLaserBlockedBefore = true;
+                }
+
+            }
+
+            if (hit.transform.tag == "LaserCube")
+            {
+                onLaserCollidedWithLaserCube.Raise(this, null, -1);
+            }
+
+            // if hitted robot
+            if (hit.transform.tag.StartsWith("Robot"))
+            {
+                // TODO: change behaviour to perform laser pointing logic only when robot is active
+                // robot activation should have the same behaviour as opening a gate,
+                // either by floor button or laser receiver
+                // check if active
+                if (!hit.transform.gameObject.GetComponent<PlayerController>().isActive)
+                {
+                    hit.transform.gameObject.GetComponent<PlayerController>().ActivateRobot();
+                    Debug.Log("Active robot please");
+                }
+                else // if not active
+                {
+                    //Debug.Log("Laser Pointing logic!");
+                }
+
+            }
+        }
+        else
+        {
+            lineRenderer.SetPosition(1, startPoint.position + direction * laserDistance );
+        }
+    }
+
+
+    private void GetSimilarReceviers()
+    {
+        if (isBlue)
+        {
+            laserReceivers = GameObject.FindGameObjectsWithTag("BlueLaserReceiver");
+        } else
+        {
+            laserReceivers = GameObject.FindGameObjectsWithTag("RedLaserReceiver");
+        }
+    }
+}
