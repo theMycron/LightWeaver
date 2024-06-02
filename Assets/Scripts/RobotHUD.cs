e8d2881458f70a51ed786f108b7951a0bd986e5c
theMycron <134844972+theMycron@users.noreply.github.com>
2024-06-02 08:54:16 +0300

    updated robot bar

--- Assets/Scripts/RobotHUD.cs
+++ Assets/Scripts/RobotHUD.cs
@@ -8,42 +8,27 @@ public class RobotHUD : MonoBehaviour
     [SerializeField] private Image qKeyImage; // Image for the Q key
     [SerializeField] private Image[] robotImages; // Array of robot images
     [SerializeField] private Image eKeyImage; // Image for the E key
-    [SerializeField] private GameObject robotBar; // Parent object of the robot images
     [SerializeField] private HorizontalLayoutGroup layoutGroup; // Layout group for automatic resizing
 
-    private List<GameObject> activeRobotImages = new List<GameObject>(); // List to track active robot images
 
     // Method to initialize the HUD based on the number of robots in the current level
     public void InitializeHUD(int numberOfRobots)
     {
+        Debug.Log("Found number of robots: " + numberOfRobots);
         // Clear any previously active robot images
-        ClearActiveRobotImages();
-
-        // Hide all robot images initially
-        foreach (var image in robotImages)
-        {
-            image.gameObject.SetActive(false);
-        }
+        ClearRobotImages();
 
         // Show the required number of robot images and add them to the active list
         for (int i = 0; i < numberOfRobots; i++)
         {
             robotImages[i].gameObject.SetActive(true);
-            activeRobotImages.Add(robotImages[i].gameObject);
         }
 
         // Hide the robot bar if there is only one robot
-        if (numberOfRobots <= 1)
-        {
-            robotBar.SetActive(false);
-        }
-        else
-        {
-            robotBar.SetActive(true);
-        }
+        layoutGroup.gameObject.SetActive(numberOfRobots > 1);
 
         // Adjust the size of the images based on the number of active images
-        AdjustImageSizes(numberOfRobots);
+        //AdjustImageSizes(numberOfRobots);
     }
 
     // Method to update the UI image of the selected robot
@@ -67,13 +52,12 @@ public class RobotHUD : MonoBehaviour
     }
 
     // Method to clear active robot images from the list
-    private void ClearActiveRobotImages()
+    private void ClearRobotImages()
     {
-        foreach (var robotImage in activeRobotImages)
+        foreach (var robotImage in robotImages)
         {
-            robotImage.SetActive(false);
+            robotImage.gameObject.SetActive(false);
         }
-        activeRobotImages.Clear();
     }
 
     // Method to adjust the size of the images based on the number of active images
